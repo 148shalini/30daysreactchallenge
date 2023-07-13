acc015161d3fa78c111f09038a0303d444f9d98b
148shalini <101161992+148shalini@users.noreply.github.com>
2023-07-13 23:19:11 +0530

    Add files via upload

---
+++ babel.js
@@ -0,0 +1,6 @@
+// JSX syntax
+// we don't need to use quotes with JSX
+
+const jsxElement = <h1>I am a JSX element</h1>;
+const welcome = <h1>Welcome to 30 Days of React Challenge</h1>;
+const data = <small>Oct 2, 2020</small>;
